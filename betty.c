3753782bc6124e91a14f6d47c6947580d467abaf
Waphie <siwaphiwehlazo@gmail.com>
2023-12-12 17:53:53 -0800

    Betty linter corrected issues

---
+++ betty.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entrance sector of the program
+ *
+ * Return: Always 0 (Success)
+ */
+void betty_main() {
+    /* code for betty */
+}
+
+int main(void)
+{
+for (;;)
+{
+        printf("Bettttttty proud\n");
+                break;
+        }
+return (0);
+}
